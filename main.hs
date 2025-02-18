d9a0ad70fb95c88cac67aa3c89706e31ca763ff9
Ghazanfar Shah Uzair <59178655+gsuzair@users.noreply.github.com>
2025-02-18 14:38:25 +0100

    setup

---
+++ main.hs
@@ -0,0 +1,7 @@
+-- 6.1.1.  The simplest sort of algebraic type is defined by enumeration. For example:
+-- data Season = Spring | Summer | Autumn | Winter
+-- deriving (Eq, Show, Enum)
+-- Program a constant function theSeasons of type [Season] containing all of the seasons in order,
+-- starting with Spring
+
+-- Enum a => a -> [a] -> on hoogle -> enumFrom :: a -> [a] can be used for this question
